21cb3c0be0038f60f606482155e1ecd7583c858f
Muhammad Hasan <hasanm9794@gmail.com>
2018-10-14 14:05:14 +0500

    Add files via upload

--- Sales Management System/main.js
+++ Sales Management System/main.js
@@ -4,36 +4,48 @@ var add = document.getElementById("add");
 console.log(add.innerText);
 add.addEventListener("click", myfunction)
 
-function pageLoad(){
-    var list = document.getElementById("u-list");
-    for(var i=0; i<= localStorage.length; i++){
-        list.appendChild(localStorage.getItem(list+i));
-        
+// var text = [];
+
+function pageLoad() {
+    for (var i = 1; i <= localStorage.length; i++) {
+        myfunction(i, true);
     }
+
 }
 
-function myfunction() {
+
+
+function myfunction(preList, flag) {
     var list = document.createElement("li");
     list.id = "list_" + id;
 
-    var text = document.getElementById('input-text').value;
+    if (flag) {
+        var text = localStorage.getItem("Text" + preList);
+    } else {
+        var text = document.getElementById('input-text').value;
+    }
+
     var textNode = document.createTextNode(text);
+    console.log(textNode);
     var divItem = document.createElement("div");
-    divItem.id = "text_"+ id;
+    divItem.id = "text_" + id;
     divItem.appendChild(textNode);
 
-    
+    if (!flag) {
+        localStorage.setItem("Text" + id, divItem.innerText);
+    }
+
     var divButtons = document.createElement("div");
     divButtons.id = "divButtons_" + id;
-    
+
     var edit = document.createElement("button");
-    edit.id = "edit_"+id;
+    edit.id = "edit_" + id;
     edit.innerHTML = "Edit";
     edit.onclick = editText;
 
-    var del =  document.createElement("button");
+    var del = document.createElement("button");
     del.innerHTML = "Delete";
-    del.id = "del_"+id;
+    del.id = "del_" + id;
     del.onclick = delText;
 
     divButtons.appendChild(edit);
@@ -44,48 +56,53 @@ function myfunction() {
     list.appendChild(divButtons);
 
     document.getElementById("u-list").appendChild(list);
+
+
     id++
-    localStorage.setItem("list"+id, list+id);
+
 }
 
-function editText(){
-    if(attempt === 1){
+function editText() {
+    if (attempt === 1) {
         return false;
     }
 
-    var editButton =  this.id;
+    var editButton = this.id;
     var editId = this.id.replace("edit_", "");
-    
+
     var textId = document.getElementById("text_" + editId);
     textId.style.display = "none";
-    
+
     var inputBox = document.createElement("input");
     inputBox.id = "input_" + id;
     inputBox.type = "text";
     inputBox.value = textId.innerText;
-    
-    var list = document.getElementById("list_"+editId);
 
-    var divButtons = document.getElementById("divButtons_" + editId );
+    var list = document.getElementById("list_" + editId);
+
+    var divButtons = document.getElementById("divButtons_" + editId);
     console.log(divButtons);
-    list.insertBefore(inputBox , divButtons );
+    list.insertBefore(inputBox, divButtons);
     inputBox.classList = "input-box";
     inputBox.focus();
     attempt = 1;
 
-    inputBox.addEventListener("keyup", function(event){
+    inputBox.addEventListener("keyup", function (event) {
         event.preventDefault();
-        if(event.keyCode === 13){
+        if (event.keyCode === 13) {
             textId.innerText = inputBox.value;
-            textId.style.display = "";  
+            textId.style.display = "";
             inputBox.style.display = "none";
+            localStorage.setItem("Text" + editId, textId.innerText);
             attempt = 0;
         }
     })
 }
 
-function delText(){
+function delText() {
     var textToDelete = this.id.replace("del_", "");
     textToDelete = document.getElementById("list_" + textToDelete);
     textToDelete.style.display = "none";
+
+    localStorage.removeItem("Text" + this.id.replace("del_",""));
 }
