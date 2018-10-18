9f7d3c3915b82ba1479f6fd615bd20a12ebe30db
Muhammad Hasan <hasanm9794@gmail.com>
2018-10-18 19:03:48 +0500

    Add files via upload

--- New folder/user.js
+++ New folder/user.js
@@ -1,19 +1,10 @@
 var todos = [];
-
+console.log("hi")
 function Todo(name) {
     this.name = name;
     this.completed = false;
 }
 
-
-// Add Todo
-function addTodoWithName(name) {
-    var t = new Todo(name);
-    console.log(t);
-    todos.push(t);
-    setData();
-    location.reload();
-}
 
 //Get Todo
 function getTodoAtIndex(index) {
@@ -48,15 +39,8 @@ function listTodos() {
         var completed = todo.completed;
         var button_Del = document.createElement("button");
         button_Del.id = "del_" + i;
-        button_Del.innerHTML = "Delete";
+        button_Del.innerHTML = "Sell Item";
         button_Del.onclick = delete_List;
-        button_Del.className = "del";
-        
-        var button_Edit = document.createElement("button");
-        button_Edit.id = "edit_" + i;
-        button_Edit.innerHTML = "Edit";
-        button_Edit.onclick = editList;
-        button_Edit.className = "edit";
 
         var span = document.createElement("span");
         span.id = "span_" + i;
@@ -70,7 +54,6 @@ function listTodos() {
 
         var divButtons = document.createElement("div");
         divButtons.id = "div_" + i;
-        divButtons.appendChild(button_Edit);
         divButtons.appendChild(button_Del);
 
         li.appendChild(divButtons);
@@ -81,18 +64,6 @@ function listTodos() {
 
     // $("#u-list").html(html);
 }
-
-//Submit button to add todo
-$("#add-todo-click").submit(function (event) {
-    event.preventDefault(); // Prevents the default behaviour
-    var name = $("#todo-name").val();
-    if (name !== "" && name !== " ") {
-        name = $("#todo-name").val();
-        //Validiate...
-        addTodoWithName(name);
-        listTodos();
-    }
-});
 
 function delete_List() {
     var index = this.id.replace("del_", "");
@@ -105,39 +76,5 @@ function delete_List() {
 //Remove Todo
 function removeTodoAtIndex(index) {
     todos.splice(index, 1);
-    setData();
-}
-
-//Edit Todo
-function editList() {
-    index = this.id.replace("edit_", "");
-    var inputbox = document.createElement("input");
-    var span = document.getElementById("span_" + index);
-    var list = document.getElementById("list_" + index);
-    var divButtons = document.getElementById("div_" + index);
-
-
-    var temp = span.innerHTML;
-    inputbox.value = temp;
-    span.style.display = "none";
-    list.insertBefore(inputbox, divButtons);
-    inputbox.focus();
-    this.style.display = "none";
-
-    inputbox.addEventListener("keyup", function (event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            span.innerHTML = inputbox.value;
-            inputbox.style.display = "none";
-            span.style.display = "";
-            editTodoAtIndex(index, inputbox.value);
-            var edit = document.getElementById("edit_" + index);
-            edit.style.display = "";
-        }
-    })
-}
-
-function editTodoAtIndex(index, newText) {
-    todos[index].name = newText;
     setData();
 }
